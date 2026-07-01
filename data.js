5e5d64dab5909f01897558b4c523deb1e14b2ca9
hktcm358-maker <hktcm358@gmail.com>
2026-07-01 14:48:40 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,62 @@
+const MASTER_DATA = [
+  {
+    id: 1,
+    word: "create",
+    meaning: "を作り出す",
+    phrase: "The artist created a beautiful poster."
+  },
+  {
+    id: 2,
+    word: "increase",
+    meaning: "増加する",
+    phrase: "The number of visitors increased last year."
+  },
+  {
+    id: 3,
+    word: "reduce",
+    meaning: "を減らす",
+    phrase: "We should reduce food waste."
+  },
+  {
+    id: 4,
+    word: "improve",
+    meaning: "を向上させる",
+    phrase: "Daily practice will improve your English."
+  },
+  {
+    id: 5,
+    word: "provide",
+    meaning: "を提供する",
+    phrase: "The website provides useful information."
+  },
+  {
+    id: 6,
+    word: "protect",
+    meaning: "を守る",
+    phrase: "A helmet can protect your head."
+  },
+  {
+    id: 7,
+    word: "suggest",
+    meaning: "を提案する",
+    phrase: "My teacher suggested a better way to study."
+  },
+  {
+    id: 8,
+    word: "prepare",
+    meaning: "を準備する",
+    phrase: "We prepared dinner before the guests arrived."
+  },
+  {
+    id: 9,
+    word: "allow",
+    meaning: "を許可する",
+    phrase: "The museum allows visitors to take photos."
+  },
+  {
+    id: 10,
+    word: "describe",
+    meaning: "を説明する",
+    phrase: "Can you describe the man you saw?"
+  }
+];
